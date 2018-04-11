10489554d827f3636db5af36809eac044517ff11
blackadder <nickharras@gmail.com>
2018-04-10 20:20:58 -0400

    Trying to create discrete lists of TCP vs UDP packets, ordered by time and ordered by port number. TCP ordered by port number is bugged.

--- Wireshark Lab 2/wireshark_02.py
+++ Wireshark Lab 2/wireshark_02.py
@@ -37,11 +37,20 @@ def main():
 
     input_data = dpkt.pcap.Reader(open(file_name,'r'))
 
+    tcp_time_list = []
+    tcp_port_list = []
+    udp_time_list = []
+    udp_port_list = []
+
+    count = 0
+
     for timestamp, packet in input_data:
         # this converts the packet arrival time in unix timestamp format
         # to a printable-string
         time_string = datetime.datetime.utcfromtimestamp(timestamp)
         
+        #print timestamp
+
         eth = dpkt.ethernet.Ethernet(packet)
         if eth.type != dpkt.ethernet.ETH_TYPE_IP:
             continue
@@ -49,14 +58,49 @@ def main():
 
         if ip.p==dpkt.ip.IP_PROTO_TCP:
             tcp = ip.data
-            print "tcp packet"
+            tcp_tuple = (timestamp, tcp)
+            time_insert(tcp_tuple, tcp_time_list)
+            port_insert(tcp, tcp_port_list)
 
         elif ip.p==dpkt.ip.IP_PROTO_UDP:
             udp = ip.data
-            print "udp packet"
+            udp_tuple = (timestamp, udp)
+            time_insert(udp_tuple, udp_time_list)
+            port_insert(udp, udp_port_list)
 
         else:
-            print "Bad packet"    
+            print "Bad packet"  
+
+        count += 1
+
+    print len(tcp_time_list)
+    print len(tcp_port_list)
+    print len(udp_time_list)
+    print len(udp_port_list)
+    print count
+    
+# Custom helper function
+# Uses a packet tuple, where tuple[0] is the timestamp of the packet, and tuple[1] is the object
+def time_insert(tuple, list):
+    if len(list) == 0 or tuple[0] >= list[len(list)-1][0]:
+        list.append(tuple)
+    else:
+        for i in range(len(list)):
+            if tuple[0] <= list[i][0] and len(tuple[1].data) > 0:
+                list.insert(i, tuple)
+                break
+            
+
+def port_insert(object, list):
+    if len(list) == 0 or object.dport >= list[len(list)-1].dport:
+        list.append(object)
+    else:
+        for i in range(len(list)):
+            if object.dport <= list[i].dport and len(object.data) > 0:
+                list.insert(i, object)
+                break
+
+
 
 # execute a main function in Python
 if __name__ == "__main__":
